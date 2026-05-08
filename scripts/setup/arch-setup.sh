db5562578463d7c072924e82ebdd34937515d463
nekrofrukt <nekrofrukt@pm.me>
2026-05-08 10:32:38 +0200

    arch setup script and waybar

--- scripts/setup/arch-setup.sh
+++ scripts/setup/arch-setup.sh
@@ -12,7 +12,7 @@ echo -e "\e[1mInstalling utils.\e[0m"
 
 # BASE UTILS
 sudo pacman -Syu
-sudo pacman -S base-devel blueman curl fastfetch firefox foot gnome-disk-utiliy gnome-text-editor man-db man-pages nano network-manager-applet ranger ripgrep starship tree ufw vim vlc wl-clipboard
+sudo pacman -S base-devel blueman curl fastfetch firefox foot gnome-disk-utiliy gnome-text-editor man-db man-pages nano network-manager-applet ranger ripgrep starship sushi tree ufw vim vlc wl-clipboard
 
 echo -e "\e[1mEnabling firewall.\e[0m"
 sudo ufw enable
