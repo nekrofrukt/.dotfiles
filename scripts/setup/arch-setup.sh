22e414e5fe8f0bbf8a78355873d49e33f5e8d0c1
nekrofrukt <nekrofrukt@pm.me>
2026-05-09 15:15:19 +0200

    arch setup script

--- scripts/setup/arch-setup.sh
+++ scripts/setup/arch-setup.sh
@@ -12,7 +12,7 @@ echo -e "\e[1mInstalling utils.\e[0m"
 
 # BASE UTILS
 sudo pacman -Syu
-sudo pacman -S base-devel blueman fastfetch firefox foot gnome-disk-utility gnome-text-editor ttf-jetbrains-mono-nerd man-db man-pages ranger ripgrep rofi starship sushi tree vlc wl-clipboard
+sudo pacman -S base-devel blueman fastfetch firefox foot gnome-disk-utility gnome-text-editor ttf-jetbrains-mono-nerd man-db man-pages ranger ripgrep rofi starship sushi tree vlc vlc-plugin-ffmpeg wl-clipboard
 
 echo -e "\e[1mEnabling firewall.\e[0m"
 sudo ufw enable
