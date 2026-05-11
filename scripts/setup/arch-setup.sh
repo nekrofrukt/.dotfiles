cf4379aa51b3dca6caf96e1c3be296ac8add0570
nekrofrukt <nekrofrukt@pm.me>
2026-05-11 13:24:45 +0200

    mako, niri, nom, etc

--- scripts/setup/arch-setup.sh
+++ scripts/setup/arch-setup.sh
@@ -12,9 +12,12 @@ echo -e "\e[1mInstalling utils.\e[0m"
 
 # BASE UTILS
 sudo pacman -Syu
-sudo pacman -S base-devel bash-completion blueman fastfetch firefox foot gnome-disk-utility gnome-text-editor ttf-jetbrains-mono-nerd man-db man-pages ranger ripgrep rofi starship sushi tree vlc vlc-plugin-ffmpeg wl-clipboard
+sudo pacman -S base-devel bash-completion blueman fastfetch firefox foot gnome-disk-utility gnome-text-editor ttf-jetbrains-mono-nerd man-db man-pages ranger ripgrep rofi starship sushi tailscale tree vlc vlc-plugin-ffmpeg wl-clipboard
 
 echo -e "\e[1mEnabling firewall.\e[0m"
 sudo ufw enable
+
+echo -e "\e[1mSetting GTK color scheme to dark.\e[0m"
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
 echo -e "\e[1mInstallation complete!\e[0m"
