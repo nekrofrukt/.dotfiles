b1189213d7967ecb4c120ee1f1f8dc0109ed2d73
nekrofrukt <nekrofrukt@pm.me>
2026-08-13 12:07:14 +0200

    nixbook flake

--- nix/hosts/nixbook/flake.nix
+++ nix/hosts/nixbook/flake.nix
@@ -1,17 +1,13 @@
 {
-  description = "Flake for managing Dell XPS 13.";
+  description = "Flake for managing Macbook Pro server.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs_stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
-    
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs_stable.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs_stable, home-manager, ... } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs_stable, ... } @ inputs:
   let
     system = "x86_64-linux";
   in {
@@ -23,7 +19,6 @@
         
 	    modules = [
           ./configuration.nix
-	      inputs.home-manager.nixosModules.default
         ];
       };
     };
